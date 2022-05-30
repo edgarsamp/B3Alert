a7bf33f531a10d43db9b782748a6e58589944c93
edgarsamp <edgar-sampaio@hotmail.com>
2022-05-30 19:56:32 -0300

    Removendo linhas de teste.

--- stock-quote-alert/Program.cs
+++ stock-quote-alert/Program.cs
@@ -1,9 +1,1 @@
-﻿using stock_quote_alert.Services;
-using stock_quote_alert.Stocks;
-
-
-ApiConsumer a = new();
-MonitoredStock stock = new("petr4", 4.0, 5.0);
-CurrentStock currentStock = ApiConsumer.GetCurrentStock(stock);
-
-Console.WriteLine(currentStock.Price);
+﻿
